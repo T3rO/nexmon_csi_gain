e653432b68ffa1d71697f44eb0e8b49b49de0998
Tristan <jacobst@students.uni-marburg.de>
2020-11-21 16:09:39 +0100

    set max analog gain 5

--- src/ioctl.c
+++ src/ioctl.c
@@ -342,7 +342,7 @@ wlc_ioctl_hook(struct wlc_info *wlc, int cmd, char *arg, int len, void *wlc_if)
             wlc_phyreg_enter(pi);
             wlc_phy_stay_in_carriersearch_acphy(pi, 1);
 
-            phy_utils_mod_phyreg(pi, 0x6e8, 0xff << 8, 55);
+            phy_reg_write(pi, 0x6e8, 0x379f);
 
             wlc_phy_stay_in_carriersearch_acphy(pi, 0);
             wlc_phyreg_exit(pi);
