e42be648c45a68db131f7d9397ab4a65cddae3ed
Tristan <jacobst@students.uni-marburg.de>
2020-11-27 14:18:42 +0100

    cp structs script dev2

--- utils/cpstructs.sh
+++ utils/cpstructs.sh
@@ -1,2 +1,2 @@
 #!/system/bin/sh
-cp ~/nexmon/patches/bcm43455c0/7_45_189/nexmon_whm/other/structs.common.h ~/nexmon/firmwares/bcm43455c0/
+cp ~/home/pi/nexmon/patches/bcm43455c0/7_45_189/nexmon_whm/other/structs.common.h ~/home/pi/nexmon/firmwares/bcm43455c0/
